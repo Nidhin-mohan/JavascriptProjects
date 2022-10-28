1c369361af2382dfa3d126a6d980baa18f1dc1be
Nidhin-mohan <nidhinmohannidhin>
2022-10-28 19:25:00 +0530

    Fixing countDown issues

--- 05CountDown/script.js
+++ 05CountDown/script.js
@@ -2,13 +2,14 @@ let Display = document.querySelector(".display");
 let year;
 let month;
 let day;
+let person;
 
 let btn = document.getElementById("btn")
 function myFunction() {
-  let person = prompt("Please enter a date ", "2022-12-01");
+   person = prompt("Please enter a date ", "2022-12-01");
  
 }
-myFunction();
+setTimeout(myFunction(), 2000);
 
 // btn.addEventListener(() => {
 //   year = document.getElementById("year").value;
@@ -24,9 +25,11 @@ myFunction();
    let endDate = new Date(person);
    let TimeLeft = endDate - today;
 
+  
+
    // console.log(endDate.getTime());
    // console.log(today.getTime());
-   // console.log(TimeLeft);
+   console.log(TimeLeft);
 
    let second = 1000;
    let minute = second * 60;
@@ -38,8 +41,18 @@ myFunction();
    let toMinute = Math.floor((TimeLeft % hour) / minute);
    let toSecond = Math.floor((TimeLeft % minute) / second);
 
-   Display.innerHTML = toDay + "d " + toHour + "hrs " + toMinute + "min " + toSecond + "sec";
-   console.log("hi");
+    
+
+   if(TimeLeft== 1000){
+    Display.innerHTML = " Count Down is ended";
+   }
+
+   if (person != "" || TimeLeft == NaN)
+     Display.innerHTML =
+       toDay + "d " + toHour + "hrs " + toMinute + "min " + toSecond + "sec";
+   else Display.innerHTML = " Enter  Date in Valid format";
+    if (TimeLeft === NaN) Display.innerHTML = " use valid format";  
+
  },1000)
 
 
